8013987ddfadf81d5239c142e819faff1a56ea63
Evgeny_Karyakin <yevgeny.karyakin@simbirsoft.com>
2024-04-19 15:36:17 +0300

    edit mysql port

--- src/test/java/org/example/jdbc/tests/JDBCTest.java
+++ src/test/java/org/example/jdbc/tests/JDBCTest.java
@@ -2,6 +2,7 @@ package org.example.jdbc.tests;
 
 import org.example.jdbc.db.AirplaneQuery;
 import org.example.jdbc.dto.AirplaneDto;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JDBCTest {
@@ -9,6 +10,6 @@ public class JDBCTest {
     @Test
     public void checkGetTestDataInDB() {
         AirplaneDto airplaneById = new AirplaneQuery().getAirplaneById(1L);
-        System.out.println(airplaneById);
+        Assert.assertNotNull(airplaneById);
     }
 }
--- src/test/java/org/example/jooq/tests/JOOQTest.java
+++ src/test/java/org/example/jooq/tests/JOOQTest.java
@@ -3,6 +3,7 @@ package org.example.jooq.tests;
 import org.example.jooq.db.AirplaneQuery;
 import org.example.jooq.db.DefaultQuery;
 import org.example.jooq.dto.AirplaneDto;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JOOQTest {
@@ -10,6 +11,6 @@ public class JOOQTest {
     @Test
     public void checkGetTestDataInDB() {
         AirplaneDto airplane = new AirplaneQuery().getAirplaneById(1L);
-        System.out.println(airplane);
+        Assert.assertNotNull(airplane);
     }
 }
